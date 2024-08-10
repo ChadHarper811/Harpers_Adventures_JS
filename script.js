ad4341c1c673da785acc4ac04284b6d3836278f1
Chad Harper <chadharper811@gmail.com>
2024-08-10 14:40:41 -0500

    more style changes and added some script changes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const data = [
     {
+        story: "Back in 2009, right after my high school graduation, my best friends Sen and Young were going to visit their family in Taiwan. I had never been outside the country at this point and I half-jokingly asked them to convince their parents to let me tag along. When they actually did ask and their parents said yes I was surprised and thrilled. I had to pay for all my own travel expenses and such but they would let me stay with them and feed me when we ate at their house. We were there for 34 days and we also went to Tokyo, Japan for 5 days. These are the better pictures below and a short journal entry for each day's activities."
+    },
+    {
         day: 1,
+        date: "7/9/2009",
+        blog: "First day of travel from Kansas City to Taiwan. Our first stop was LA ,which took about 4 hours but with the time zones it took it down to about 1 hour on paper (9:00 am KC to 10:30 am LA), then left LA on a bigger plane that would take us to Taiwan at 4pm. The flight from LA to Taiwan was 13 hours but again time zone changes made it much less and we landed around 8:30 pm the next day (time zones are weird) just as the sun was setting.",
         dayData: [
             {
                 image: 1,
@@ -11,6 +16,8 @@ const data = [
     },
     {
         day: 2,
+        date: "7/10/2009",
+        blog: "We went from the airport in Taipei to my friend's grandma's house in Keelung, where we would call home for most of the trip, and passed out immediately. Overall we were awake and in the sun for almost 23 hours that day. The time zone changes made it a very long day, or 2 days I suppose, since we were going with the sun so this was probably the longest day of my life.",
         dayData: [
             {
                 image: 1,
@@ -21,6 +28,8 @@ const data = [
     },
     {
         day: 3,
+        date: "7/11/2009",
+        blog: "First official day we got up at a decent time around 8am and walked into town to see a bit and buy some food. After walking around a while we go back to the house and take a nap before setting out for the town again at night to go shopping at the night market.",
         dayData: [
             {
                 image: 1,
@@ -46,6 +55,8 @@ const data = [
     },
     {
         day: 4,
+        date: "7/12/2009",
+        blog: "Today we have plans to explore some more of the town nearby again and see a concert on the beach a decent drive away. So we started with going to town to get food again. This time I tried some new fruit called guava and pitaya (also called dragon fruit), both are delicious. You can get these in the states but nothing compares to local and fresh since they have to pick them early to ship them, they just don’t compare. After breakfast we head up the hill to a local park/temple and get a beautiful view of Keelung. We rested a while and then headed down the coast to Fulong Beach for the concert. The stage is set on a huge sand barge and there are hundreds of people. My friend's aunt doesn’t let me swim because it’s typhoon season but I still play in the water and have a great time at the concert before we head home for the night.",
         dayData: [
             {
                 image: 1,
@@ -71,6 +82,8 @@ const data = [
     },
     {
         day: 5,
+        date: "7/13/2009",
+        blog: "We go into town looking for a place to get a phone sim card for the brothers so they can call/text while we are here without international charges. After we find that, we get dropped off in Taipei and visit one of my friend's uncle's shops and they catch up for a while. We then go and get our hair done. Both the brothers get a bit cut and styled with one even getting red dye while I try and see what dark hair looks like on me, they don’t let me get black so I go with dark brown (it did not look that good but at the time I thought it was cool, should have taken the hint from the stylist). We then meet up with my friend's aunt at her work and try to chat in English with some of their coworkers' daughters to help them practice. We head home after that.",
         dayData: [
             {
                 image: 1,
@@ -81,6 +94,8 @@ const data = [
     },
     {
         day: 6,
+        date: "7/14/2009",
+        blog: "Today we are heading back to Taipei to visit another of my friends' uncles, aunt and cousins. This time we take the bus into the city and then hop on the train to get there (my first time on a train ever!). We hang out with their cousins till dinner and all go out to a nice restaurant where I get my fill of sushi and pork. We then go to the local night market and buy some clothes. We stayed the night with their cousins.",
         dayData: [
             {
                 image: 1,
@@ -101,6 +116,8 @@ const data = [
     },
     {
         day: 7,
+        date: "7/15/2009",
+        blog: "Woke up and had breakfast with the uncle and cousins then took a train to Ximen to buy some anime stuff and site see. We then met up with their other relative (his grandpa’s sister’s daughter, they have a lot of family) and went to visit their house. They lived in a huge building up on a high floor with an awesome view of the city nearby. We all hung out for a while and visited with the family and kids. After that we went back to the Shilin night market and looked around again, ate some amazing street food and shaved ice. After that we got picked up and went back home.",
         dayData: [
             {
                 image: 1,
@@ -121,6 +138,8 @@ const data = [
     },
     {
         day: 8,
+        date: "7/16/2009",
+        blog: "Today was a free day with no plans so we lazed around and had some awesome home cooked food.",
         dayData: [
             {
                 image: 1,
@@ -131,6 +150,8 @@ const data = [
     },
     {
         day: 9,
+        date: "7/17/2009",
+        blog: "Today was my favorite, going to the tallest tower in the world! (was the tallest during this trip) We started walking to the bus and taking it into Taipei to see Harry Potter 6, it was ok. After that, we headed over to the Taipei 101 tower. We made a bank visit while on the way to get more cash and went to a NT$39 store, a 39 taiwan dollar store which is like a dollar store in the states, and got some souvenirs. Once at the tower we head up to the top of the roof on the 91st floor but only get to see half the roof since it's windy and still typhoon season. It was the most amazing view and I could see all the way to the mountains. We stay till sun down and go to a bookstore on the way back home and pass out after a long day.",
         dayData: [
             {
                 image: 1,
@@ -156,6 +177,8 @@ const data = [
     },
     {
         day: 10,
+        date: "7/18/2009",
+        blog: "Same usual morning start but today we went to an anime event in town. Lots of people were in cosplay and there were lots of vendors and cool stuff all over. We spent a few hours there looking around and then headed to Taipei. This time we went to the Taipei world trade center to see a concert. It was awesome but I really didn't understand any of it cause I don’t speak chinese but it was still fun to watch. Headed home after that.",
         dayData: [
             {
                 image: 1,
@@ -179,29 +202,37 @@ const data = [
             }
         ]
     },
-    {
-        day: ,
-        dayData: [
-            {
-                image: 1,
-                src: "travel_pictures/7-1-09 1.JPG",
-                caption: ""
-            },
-            {
-                image: 2,
-                src: "travel_pictures/7-1-09 2.JPG",
-                caption: ""
-            },
-            {
-                image: 3,
-                src: "travel_pictures/7-1-09 3.JPG",
-                caption: ""
-            },
-            {
-                image: 4,
-                src: "travel_pictures/7-1-09 4.JPG",
-                caption: ""
-            }
-        ]
-    },
+    // {
+    //     day: ,
+    //     date: ,
+    //     blog: "",
+    //     dayData: [
+    //         {
+    //             image: 1,
+    //             src: "travel_pictures/7-1-09 1.JPG",
+    //             caption: ""
+    //         },
+    //         {
+    //             image: 2,
+    //             src: "travel_pictures/7-1-09 2.JPG",
+    //             caption: ""
+    //         },
+    //         {
+    //             image: 3,
+    //             src: "travel_pictures/7-1-09 3.JPG",
+    //             caption: ""
+    //         },
+    //         {
+    //             image: 4,
+    //             src: "travel_pictures/7-1-09 4.JPG",
+    //             caption: ""
+    //         }
+    //     ]
+    // },
 ]
+
+document.getElementById("travel-story").innerHTML += `<p>${data[0].story}</p>`;
+
+const gallery = document.getElementById("gallery");
+const travelStory = document.getElementById("travel-story");
+const travelBlog = document.getElementById("travel-blog")
