c63ab76d9250addf7fea0ca4b6bb92a77fbeb1e9
Chad Harper <chadharper811@gmail.com>
2024-08-10 13:20:33 -0500

    added more to data object up to day 10

--- script.js
+++ script.js
@@ -44,5 +44,139 @@ const data = [
             }
         ]
     },
-
+    {
+        day: 4,
+        dayData: [
+            {
+                image: 1,
+                src: "travel_pictures/7-12-09 1.JPG",
+                caption: "Zhongzheng Park Entrance"
+            },
+            {
+                image: 2,
+                src: "travel_pictures/7-12-09 2.JPG",
+                caption: "Highway mountain pass along the coast"
+            },
+            {
+                image: 3,
+                src: "travel_pictures/7-12-09 3.JPG",
+                caption: "Fulong Beach"
+            },
+            {
+                image: 4,
+                src: "travel_pictures/7-12-09 4.JPG",
+                caption: "Fulong Beach concert"
+            }
+        ]
+    },
+    {
+        day: 5,
+        dayData: [
+            {
+                image: 1,
+                src: "travel_pictures/7-13-09 1.JPG",
+                caption: "random street in Taipei"
+            }
+        ]
+    },
+    {
+        day: 6,
+        dayData: [
+            {
+                image: 1,
+                src: "travel_pictures/7-14-09 1.JPG",
+                caption: "Bus from Keelung to Taipei"
+            },
+            {
+                image: 2,
+                src: "travel_pictures/7-14-09 2.JPG",
+                caption: "Taipei subway station"
+            },
+            {
+                image: 3,
+                src: "travel_pictures/7-14-09 3.JPG",
+                caption: "A night market in Taipei"
+            }
+        ]
+    },
+    {
+        day: 7,
+        dayData: [
+            {
+                image: 1,
+                src: "travel_pictures/7-15-09 1.JPG",
+                caption: "Party World Building - Taipei Ximending"
+            },
+            {
+                image: 2,
+                src: "travel_pictures/7-15-09 2.JPG",
+                caption: "Ximending city view"
+            },
+            {
+                image: 3,
+                src: "travel_pictures/7-15-09 3.JPG",
+                caption: "Shilin night market"
+            }
+        ]
+    },
+    {
+        day: 8,
+        dayData: [
+            {
+                image: 1,
+                src: "travel_pictures/7-16-09 1.JPG",
+                caption: "Lazy day with some miso soup"
+            }
+        ]
+    },
+    {
+        day: 9,
+        dayData: [
+            {
+                image: 1,
+                src: "travel_pictures/7-17-09 1.JPG",
+                caption: "Bus from Keelung to Taipei"
+            },
+            {
+                image: 2,
+                src: "travel_pictures/7-17-09 2.JPG",
+                caption: "Taipei 101 Tower"
+            },
+            {
+                image: 3,
+                src: "travel_pictures/7-17-09 3.JPG",
+                caption: "View from the top of Taipei 101"
+            },
+            {
+                image: 4,
+                src: "travel_pictures/7-17-09 4.JPG",
+                caption: "Sunset view from top of Taipei 101"
+            }
+        ]
+    },
+    {
+        day: 10,
+        dayData: [
+            {
+                image: 1,
+                src: "travel_pictures/7-18-09 1.JPG",
+                caption: "Street anime convention in Ximen"
+            },
+            {
+                image: 2,
+                src: "travel_pictures/7-18-09 2.JPG",
+                caption: "Pin Guan Concert"
+            },
+            {
+                image: 3,
+                src: "travel_pictures/7-18-09 3.JPG",
+                caption: "Bowl of Ramen"
+            },
+            {
+                image: 4,
+                src: "travel_pictures/7-18-09 4.JPG",
+                caption: "Collage of National Sun yat-sen Memorial Hall"
+            }
+        ]
+    },
 ]
