4af9c435123bd062310c89ef8185abe33eedd1c5
Chad Harper <chadharper811@gmail.com>
2024-08-10 13:08:26 -0500

    more style changes and adding array of data to replace hard code with js object added up to day 3

---
+++ script.js
@@ -0,0 +1,48 @@
+const data = [
+    {
+        day: 1,
+        dayData: [
+            {
+                image: 1,
+                src: "travel_pictures/7-09-09.JPG",
+                caption: "China International Plane"
+            }
+        ]
+    },
+    {
+        day: 2,
+        dayData: [
+            {
+                image: 1,
+                src: "travel_pictures/7-10-09.JPG",
+                caption: "13 hours in this plane seat"
+            }
+        ]
+    },
+    {
+        day: 3,
+        dayData: [
+            {
+                image: 1,
+                src: "travel_pictures/7-11-09 1.JPG",
+                caption: "Keelung, Taiwan"
+            },
+            {
+                image: 2,
+                src: "travel_pictures/7-11-09 2.JPG",
+                caption: "Keelung, Taiwan"
+            },
+            {
+                image: 3,
+                src: "travel_pictures/7-11-09 3.JPG",
+                caption: "Random building in Keelung"
+            },
+            {
+                image: 4,
+                src: "travel_pictures/7-11-09 4.JPG",
+                caption: "Keelung night market"
+            }
+        ]
+    },
+
+]
